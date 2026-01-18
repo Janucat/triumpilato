0ce845aff1e49720aff8c85554db1e57696f35a6
Janucat <jan.rovatti@gmail.com>
2026-01-18 18:04:03 +0100

    new cahpters

--- dark_mode.js
+++ dark_mode.js
@@ -9,3 +9,23 @@ button.addEventListener("click", () => {
     button.textContent = "dark mode";
     }
 });
+
+
+const backToTopBtn = document.getElementById("backToTop");
+
+// Mostra/nasconde il bottone
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 200) {
+    backToTopBtn.style.display = "block";
+  } else {
+    backToTopBtn.style.display = "none";
+  }
+});
+
+// Torna in cima
+backToTopBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
