ec4f6ba802d75068524866ed837a41593987935a
Janucat <jan.rovatti@gmail.com>
2026-04-20 12:55:13 +0200

    messo in ordine

--- scripts/common.js
+++ scripts/common.js
@@ -1,16 +1,3 @@
-const button = document.getElementById("toggleBtn");
-
-button.addEventListener("click", () => {
-    document.body.classList.toggle("dark-mode");
-
-    if (document.body.classList.contains("dark-mode")) {
-    button.textContent = "light mode";
-    } else {
-    button.textContent = "dark mode";
-    }
-});
-
-
 const backToTopBtn = document.getElementById("backToTop");
 
 // Mostra/nasconde il bottone
